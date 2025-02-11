a6191fd818c2a8534cbc339dd06656169460f22d
Mehdy-H-Asaad <mehdyasaad.sy.2003@gmail.com>
2025-02-11 16:51:09 +0400

    Location Fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Admin } from "./pages/Admin";
 function App() {
